c336fb4fb89c6f5209a5d95cb1b98c09b0958338
ivandiaz-fillthegapp <94384055+ivandiaz-fillthegapp@users.noreply.github.com>
2026-04-14 21:09:38 +0200

    Implement Hello World component

---
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/HelloWorld.kt
@@ -0,0 +1,9 @@
+package com.example.android.architecture.blueprints.todoapp
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HelloWorld() {
+    Text(text = "Hello, World!")
+}
