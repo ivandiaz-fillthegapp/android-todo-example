03d43516575e4ed9e7aa5319c05d1a3d462560ea
ivandiaz-fillthegapp <94384055+ivandiaz-fillthegapp@users.noreply.github.com>
2026-04-17 21:16:00 +0200

    Create UI test for HelloWorldScreen

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/HelloWorldScreenTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/HelloWorldScreenTest.kt
@@ -3,6 +3,7 @@ package com.example.android.architecture.blueprints.todoapp
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.setContent
 import androidx.compose.ui.test.performClick
 import org.junit.Rule
 import org.junit.Test
