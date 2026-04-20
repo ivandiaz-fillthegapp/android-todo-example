8f5a8945c33ccca71c7b710a9706e44dce68706e
ivandiaz-fillthegapp <94384055+ivandiaz-fillthegapp@users.noreply.github.com>
2026-04-20 21:37:06 +0200

    Add unit tests for HelloWorldScreen

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/HelloWorldScreenTest.kt
@@ -0,0 +1,24 @@
+package com.example.android.architecture.blueprints.todoapp
+
+import androidx.compose.material.Text
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.performClick
+import org.junit.Rule
+import org.junit.Test
+
+class HelloWorldScreenTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun helloWorldScreen_displaysHelloWorld() {
+        composeTestRule.setContent {
+            HelloWorldScreen()
+        }
+
+        composeTestRule.onNodeWithText("Hello World")
+            .assertIsDisplayed()
+    }
+}
