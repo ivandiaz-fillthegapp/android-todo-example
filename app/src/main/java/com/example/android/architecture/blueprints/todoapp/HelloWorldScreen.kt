4b2732f854c84d08a6ad696cd1464ffe2f05adeb
ivandiaz-fillthegapp <94384055+ivandiaz-fillthegapp@users.noreply.github.com>
2026-04-20 15:08:58 +0200

    Create HelloWorldScreen Composable

---
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/HelloWorldScreen.kt
@@ -0,0 +1,9 @@
+package com.example.android.architecture.blueprints.todoapp
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HelloWorldScreen() {
+    Text(text = "Hello, World!")
+}
