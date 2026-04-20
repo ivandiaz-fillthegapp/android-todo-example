0a2c37ab4dd4e7a254e90e5ba65046aa0e58c196
ivandiaz-fillthegapp <94384055+ivandiaz-fillthegapp@users.noreply.github.com>
2026-04-20 21:40:40 +0200

    Add test for HelloWorldScreen

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/HelloWorldScreenTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/HelloWorldScreenTest.kt
@@ -3,19 +3,17 @@ package com.example.android.architecture.blueprints.todoapp
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.setContent
-import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.junit4.ComposeTestRule
 import org.junit.Rule
 import org.junit.Test
 
 class HelloWorldScreenTest {
-
     @get:Rule
     val composeTestRule = createComposeRule()
 
     @Test
     fun helloWorldScreen_displaysHelloWorld() {
         composeTestRule.setContent { HelloWorldScreen() }
-        composeTestRule.onNodeWithText("Hello World!").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Hello, World!").assertIsDisplayed()
     }
 }
