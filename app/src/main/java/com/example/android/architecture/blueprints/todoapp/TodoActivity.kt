2c98b6c166a674ad06df53b50190c6dc47746934
ivandiaz-fillthegapp <94384055+ivandiaz-fillthegapp@users.noreply.github.com>
2026-04-21 15:42:23 +0200

    Modify TodoActivity to display HelloWorldScreen

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoActivity.kt
@@ -3,18 +3,14 @@ package com.example.android.architecture.blueprints.todoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import com.example.android.architecture.blueprints.todoapp.ui.theme.TodoAppTheme
 
 class TodoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { 
-            TodoAppTheme { 
-                Surface(color = MaterialTheme.colors.background) {
-                    HelloWorldScreen()  // Set HelloWorldScreen as content
-                }
+        setContent {
+            TodoAppTheme {
+                HelloWorldScreen() // Display the Hello World screen
             }
         }
     }
