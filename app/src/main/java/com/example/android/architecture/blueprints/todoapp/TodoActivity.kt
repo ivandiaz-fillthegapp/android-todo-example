f77055c53b1c3c249586cd882b4cc7ce58d48a72
ivandiaz-fillthegapp <94384055+ivandiaz-fillthegapp@users.noreply.github.com>
2026-04-21 09:11:50 +0200

    Integrate HelloWorldScreen into TodoActivity to display it when the app starts.

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoActivity.kt
@@ -1,11 +1,22 @@
-import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.activity.viewModels
+package com.example.android.architecture.blueprints.todoapp
 
-class TodoActivity : AppCompatActivity() {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import com.example.android.architecture.blueprints.todoapp.ui.theme.TodoAppTheme
+
+class TodoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { SimpleHelloWorld() }
+        setContent { 
+            TodoAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    HelloWorldScreen()
+                }
+            }
+        }
     }
 }
