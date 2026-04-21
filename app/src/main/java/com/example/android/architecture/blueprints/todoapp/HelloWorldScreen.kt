bc6b863a283f0a208f0b0b60a43a67b0be101e8a
ivandiaz-fillthegapp <94384055+ivandiaz-fillthegapp@users.noreply.github.com>
2026-04-21 14:59:43 +0200

    Define Hello World Composable function

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/HelloWorldScreen.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/HelloWorldScreen.kt
@@ -2,15 +2,8 @@ package com.example.android.architecture.blueprints.todoapp
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun HelloWorldScreen() {
     Text(text = "Hello World")
-}
-
-@Preview
-@Composable
-fun PreviewHelloWorldScreen() {
-    HelloWorldScreen()
 }
