fcfa16075edec6dcb838409e004bbfbf2cb71cea
ivandiaz-fillthegapp <94384055+ivandiaz-fillthegapp@users.noreply.github.com>
2026-04-21 12:25:40 +0200

    Integrate HelloWorldScreen into navigation graph.

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoNavGraph.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoNavGraph.kt
@@ -2,6 +2,7 @@ package com.example.android.architecture.blueprints.todoapp
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.example.android.architecture.blueprints.todoapp.HelloWorldScreen
 
 fun NavGraphBuilder.todoNavGraph() {
     composable(route = "hello") { HelloWorldScreen() }
