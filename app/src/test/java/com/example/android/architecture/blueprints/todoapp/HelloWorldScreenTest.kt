28654033aeb1da7f6bdbae54ed9fd81786d08432
ivandiaz-fillthegapp <94384055+ivandiaz-fillthegapp@users.noreply.github.com>
2026-04-21 15:42:28 +0200

    Add unit test for HelloWorldScreen

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/HelloWorldScreenTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/HelloWorldScreenTest.kt
@@ -1,27 +1,20 @@
 package com.example.android.architecture.blueprints.todoapp
 
-import androidx.compose.material.ContentAlpha
-import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.assertIsDisplayed
 import org.junit.Rule
 import org.junit.Test
 
 class HelloWorldScreenTest {
+
     @get:Rule
     val composeTestRule = createComposeRule()
 
     @Test
     fun helloWorldScreen_displaysHelloWorld() {
-        composeTestRule.setContent { 
-            MaterialTheme { 
-                HelloWorldScreen()
-            } 
-        }
-        // Verify that the text 'Hello World' is displayed
-        composeTestRule.onNodeWithText("Hello World", useUnmergedTree = true)
-            .assertIsDisplayed()
+        composeTestRule.setContent { HelloWorldScreen() }
+        composeTestRule.onNodeWithText("Hello World").assertIsDisplayed()
     }
 }
