6a773179a5b613a9838a8e3eac93549d99f17c7d
ivandiaz-fillthegapp <94384055+ivandiaz-fillthegapp@users.noreply.github.com>
2026-04-21 09:16:13 +0200

    Create Hello World screen using Jetpack Compose

---
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/HelloWorldScreen.kt
@@ -0,0 +1,16 @@
+package com.example.android.architecture.blueprints.todoapp
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HelloWorldScreen() {
+    Text(text = "Hello World")
+}
+
+@Preview
+@Composable
+fun PreviewHelloWorldScreen() {
+    HelloWorldScreen()
+}
