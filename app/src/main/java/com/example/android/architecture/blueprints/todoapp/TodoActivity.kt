cf26101ff9b545352b5a29791ca4f939447f8bca
ivandiaz-fillthegapp <94384055+ivandiaz-fillthegapp@users.noreply.github.com>
2026-04-21 07:54:52 +0200

    Update TodoActivity to navigate to HelloWorldScreen

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoActivity.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/TodoActivity.kt
@@ -1,11 +1,25 @@
-import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.activity.viewModels
+package com.example.android.architecture.blueprints.todoapp
 
-class TodoActivity : AppCompatActivity() {
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class TodoActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { SimpleHelloWorld() }
+        setContent { 
+            Surface(color = MaterialTheme.colors.background) {
+                HelloWorldScreen()
+            }
+        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    HelloWorldScreen()
 }
